9eb55b32da3d90f9e87ceed1a543ba4c26b6f7b9
Amber Taranto <ambzzzz@gmail.com>
2019-09-18 23:02:47 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,15 +6,18 @@ class Show < ActiveRecord::Base
         Show.maximum("rating")
     end
 
-    # def self.most_popular_show
-    #     rating = Show.highest_rating
-    #     show = Show.find_by(rating: rating)
-    #     show
-    # end
-    #
     def self.most_popular_show
-        Show.order("rating DESC").limit(1).first
+        rating = Show.highest_rating
+        show = Show.find_by(rating: rating)
+        show
     end
 
+    # def self.most_popular_show
+    #     Show.order("rating DESC").limit(1).first
+    # end
+
+    def self.lowest_rating
+        Show.minimum("rating")
+    end
 
 end
